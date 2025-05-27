ddac61492d352b78e36b1aee84628783e2adf8c3
Jaqueline <delgadillojacqueline@gmail.com>
2025-05-27 17:38:51 -0300

    guardar cambios

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Button from "./components/button";
+function App() {
+  return (
+    <>
+      <Button />;
+      <Navbar />
+      <ItemListContainer />
+      <Counter />
+    </>
+  );
+}
+export default App;
---
+++ src/components/counter.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+function Counter() {
+  const [count, setCount] = useState(0);
+  const handleRestar = () => setCount(count - 1);
+  const handleSumar = () => setCount(count + 1);
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: 10,
+        width: 400,
+      }}
+    >
+      <p style={{ border: "solid white 1px", padding: 10, borderRadius: 5 }}>
+        {count}
+      </p>
+      <div style={{ display: "flex", gap: 10 }}>
+        <button style={{ width: "50%" }} onClick={handleRestar}>
+          {" "}
+          -{" "}
+        </button>
+        <button style={{ width: "50%" }} onClick={handleSumar}>
+          {" "}
+          +{" "}
+        </button>
+      </div>
+    </div>
+  );
+}
