bcc97b6179ed281df489840e8a779692c1cec3e9
Jaqueline <delgadillojacqueline@gmail.com>
2025-07-31 02:10:51 -0300

    guardar cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
 import "./App.css";
 import NavBar from "./components/navbar";
 import { Routes } from "react-router-dom";
+import NavBar from "./components/navbar";
+import Productos from "./components/Productos";
+import Resumen from "./components/Resumen";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <NavBar />
-      <Routes>{}</Routes>
-    </>
+      <Routes>
+        <Route path="/" element={<Productos />} />
+        <Route path="/resumen" element={<Resumen />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
 export default App;
 
 
